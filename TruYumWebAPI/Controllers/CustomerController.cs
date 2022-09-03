a564ce193ad2a0a5c0462958f8705e542c27d254
Revathi <revathisundaram38@gmail.com>
2022-09-03 23:25:10 +0530

    Truyum
    New file

---
+++ TruYumWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruYum.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Customer")]
+    public class CustomerController : ControllerBase
+    {
+        private static List<Cart> carts = new List<Cart>();
+
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            var menu = AdminController.ls;
+            List<MenuItem> list = new List<MenuItem>();
+            foreach (var x in menu)
+            {
+                if (x.Active == true && x.DateOfLaunch < DateTime.Now)
+                {
+                    list.Add(x);
+                }
+            }
+            return list;
+
+        }
+
+
+        [HttpGet("{UserId}")]
+        public IEnumerable<Cart> Get(int UserId)
+        {
+            return carts.Where(c => c.userId == UserId);  // Total Price is Not included
+        }
+
+        [HttpPost]
+        public string Post([FromBody] Cart item)
+        {
+            
+            carts.Add(item);
+            return "Added to Cart";
+        }
+
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            
+            carts.Remove(carts.Where(c => c.id == id).FirstOrDefault());
+            return "Removed from Cart";
+        }
+    }
+}
